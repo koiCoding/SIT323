71c91b8f011fff6ab0c2b0fcc76c171c216168ed
koiCoding <mkpha.contact@gmail.com>
2024-03-29 10:47:55 +1100

    Update server.js

--- Week 4/server.js
+++ Week 4/server.js
@@ -27,6 +27,7 @@ const logger = winston.createLogger({
       format: winston.format.simple(),
     }));
   }
+//arithmetic functions to handle request for addition, subtraction, multiplication, and division
 const add= (n1,n2) => {
     return n1+n2;
 }
@@ -39,10 +40,12 @@ const multiply=(n1,n2) =>{
 const div=(n1,n2) =>{
     return n1/n2;
 }
+//get methods for each function including request and response
 app.get("/add", (req,res)=>{
     try{
     const n1= parseFloat(req.query.n1);
     const n2= parseFloat(req.query.n2);
+    //throuw expected failures manually
     if(isNaN(n1)) {
         logger.error("n1 is incorrectly defined");
         throw new Error("n1 incorrectly defined");
@@ -51,9 +54,11 @@ app.get("/add", (req,res)=>{
         logger.error("n2 is incorrectly defined");
         throw new Error("n2 incorrectly defined");
     }
-    
+    //through logging, keep a record of successful and failures cases.
     logger.info('Parameters '+n1+' and '+n2+' received for addition');
     const result = add(n1,n2);
+
+    //status code corresponding to data result of different functions
     res.status(200).json({statuscocde:200, data: result }); 
     } catch(error) { 
         console.error(error)
