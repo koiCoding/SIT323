b60cfcf8069a45f10ebd6350459de3e68e2744a3
koiCoding <mkpha.contact@gmail.com>
2024-03-29 11:49:07 +1100

    Task 4.2C

--- Week 4/server.js
+++ Week 4/server.js
@@ -40,6 +40,22 @@ const multiply=(n1,n2) =>{
 const div=(n1,n2) =>{
     return n1/n2;
 }
+const expo=(n1,n2) =>{
+    let result = 1; // initialize a variable to assign result value after n2 times of self-multiplication of n1
+    if(n2 == 0){
+        return 1;
+    }
+    for(i=0;i < n2;i++){
+        result *= n1;
+    }
+    return result;
+}
+const sqrt=(n1)=>{
+    return Math.sqrt(n1);
+}
+const mod=(n1,n2)=>{
+    return n1%n2;
+}
 //get methods for each function including request and response
 app.get("/add", (req,res)=>{
     try{
@@ -83,7 +99,7 @@ app.get("/sub", (req,res)=>{
     res.status(210).json({statuscocde:210, data: result }); 
     } catch(error) { 
         console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
+        res.status(510).json({statuscocde:510, msg: error.toString() })
       }
 });
 app.get("/multiply", (req,res)=>{
@@ -104,7 +120,7 @@ app.get("/multiply", (req,res)=>{
     res.status(220).json({statuscocde:220, data: result }); 
     } catch(error) { 
         console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
+        res.status(520).json({statuscocde:520, msg: error.toString() })
       }
 });
 app.get("/divide", (req,res)=>{
@@ -129,7 +145,71 @@ app.get("/divide", (req,res)=>{
     res.status(230).json({statuscocde:230, data: result }); 
     } catch(error) { 
         console.error(error)
-        res.status(500).json({statuscocde:500, msg: error.toString() })
+        res.status(530).json({statuscocde:530, msg: error.toString() })
+      }
+});
+app.get("/expo", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2= parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    if(n2 < 0 || !(Number.isInteger(n2))){
+        logger.error("n2 is invalid input!");
+        throw new Error("n2 invalid!");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for exponentiation');
+    const result = expo(n1,n2);
+    res.status(240).json({statuscocde:240, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(540).json({statuscocde:540, msg: error.toString() })
+      }
+});
+app.get("/sqrt", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    logger.info('Parameters '+n1+' received for square root');
+    const result = sqrt(n1);
+    res.status(250).json({statuscocde:250, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(550).json({statuscocde:550, msg: error.toString() })
+      }
+});
+app.get("/mod", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2= parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    if(n2 == 0)
+    {
+        logger.error("Result does not exist!");
+        throw new Error("Result does not exist!")
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for modulo operation');
+    const result = mod(n1,n2);
+    res.status(260).json({statuscocde:260, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(560).json({statuscocde:560, msg: error.toString() })
       }
 });
 const port=3040;
